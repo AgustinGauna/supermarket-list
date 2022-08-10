50ae8505086a62347bd0fb6f04022d6525cfcc23
AgustinGauna <agustingauna1997@gmail.com>
2022-08-10 19:59:27 -0300

    Primera version

---
+++ case-1/src/App.tsx
@@ -0,0 +1,71 @@
+import type {Item} from "./types";
+
+import {useEffect, useState} from "react";
+
+import styles from "./App.module.scss";
+import api from "./api";
+
+interface Form extends HTMLFormElement {
+  text: HTMLInputElement;
+}
+
+function App() {
+  const [items, setItems] = useState<Item[]>([]);
+  const [idd,setIdd] = useState<any>()
+
+  function handleToggle(id: Item["id"]) {
+    setItems((items) =>
+      items.map((item) => (item.id === id ? {...item, completed: !item.completed} : item)),
+    );
+  }
+
+  function guidGenerator() {
+    var S4 = function () {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}
+
+  function handleAdd(event: React.ChangeEvent<Form>) {
+    const newId = guidGenerator()
+    setIdd(newId)
+    event.preventDefault();
+    const newItem = event.target.text.value
+    const newItems = [...items, {text: newItem, id: idd, completed: false}]
+    setItems(newItems)
+
+    
+    // Should implement
+  }
+
+  function handleRemove(id: Item["id"]) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  useEffect(() => {
+    api.list().then(setItems);
+  }, []);
+
+  return (
+    <main className={styles.main}>
+      <h1>Supermarket list</h1>
+      <form onSubmit={handleAdd}>
+        <input name="text" type="text" />
+        <button>Add</button>
+      </form>
+      <ul>
+        {items?.map((item) => (
+          <li
+            key={item.id}
+            className={item.completed ? styles.completed : ""}
+            onClick={() => handleToggle(item.id)}
+          >
+            {item.text} <button onClick={() => handleRemove(item.id)}>[X]</button>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+export default App;
